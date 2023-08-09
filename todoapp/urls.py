79572c26b8f03864f51c1e1e09236a7ee14ed733
Clinton Nwachukwu <Clintonmatics@gmail.com>
2023-08-09 08:24:39 +0100

    initial commit

---
+++ todoapp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path 
+from . import views 
+
+
+urlpatterns = [
+    path("todos", views.todo_list),
+    path("todos/<int:pk>", views.todo_detail)
+]
+
+
+# urls:
+    # http://127.0.0.1:8000/todos
+    # http://127.0.0.1:8000/todos/id
